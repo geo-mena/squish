8ed09109f65e7a515896a55727b69391083318c2
Geovanny Mena <geovannymenavc@gmail.com>
2025-01-21 18:44:02 -0500

    ✨ Adjust small Button component size for improved consistency

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2",
-        sm: "h-7 rounded-md px-3 text-xs", // h-8 antes
+        sm: "h-6 w-8 rounded-md px-3 text-xs", // h-8 antes, w-8 no existía
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
       },
